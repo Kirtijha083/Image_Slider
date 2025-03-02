1e7978fce8db0f45b196c89bc166afdf745c482e
KirtiJha <kirti.jha2025@gmail.com>
2025-03-02 12:12:10 +0530

    adding new fratures

--- script.js
+++ script.js
@@ -6,6 +6,43 @@ const images = document.querySelectorAll(".image");
 let slideNumber = 1; // in statring value of slide is 1 and img 1 is showing
 const length = images.length; // storing images.length in (length variable)
 
+
+// creating dot btn with js dom
+const button = document.querySelector(".button");
+for(let i = 0; i<length; i++){
+  const div = document.createElement("div");
+  div.className = "btn";
+  button.appendChild(div); //mean button div ke ander chid div bnega btn naam(class) ka
+}
+
+const btns = document.querySelectorAll(".btn");
+
+btns[0].style.backgroundColor = "white";
+
+// bgcolor on btn
+const resetBg = () => {
+  btns.forEach((btn) =>{
+    btn.style.backgroundColor = "transparent";
+  })
+}
+
+// on clicking dot btn
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    resetBg();
+    slider.style.transform = `translateX(-${i*800}px)`;
+    slideNumber = i+1; //conecting both dot-btn and arrow
+    btn.style.backgroundColor = "white";
+  });
+});
+
+
+const changeColor = () => {
+  resetBg();
+  btns[slideNumber - 1].style.backgroundColor = "white";
+}; //this function is for connecting arrow and dots
+
+// this is for arrow btn...
 // create a function for nxt slide.....
 const nextSlide = () => { //fun for right arrow
   slider.style.transform = `translateX(-${slideNumber * 800}px)`; //then add css style->property(transform)...size of img(width =800px)..so multiply slidenumber with 800px..then img will change
@@ -35,6 +72,7 @@ right.addEventListener("click", () => { //then add eventlistener in right arrow
     } else {
         getFirstSlide();
     }
+    changeColor();
 });
 // also write this with the help of terninary operator like
 // right.addEventListener("click", () => {
@@ -47,4 +85,7 @@ left.addEventListener("click", () => {
     } else {
         getLastSlide();
     }
+    changeColor();
 });
+
+
