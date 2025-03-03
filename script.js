b859631893de067e4ecfb91359931196445c918a
KirtiJha <kirti.jha2025@gmail.com>
2025-03-03 19:52:45 +0530

    adding responsive frature in js

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ let slideNumber = 1; // in statring value of slide is 1 and img 1 is showing
 const length = images.length; // storing images.length in (length variable)
 
 
+// Function to get image width based on screen size
+const getImageWidth = () => {
+  if (window.innerWidth <= 480) return 350; // Small Phones
+  if (window.innerWidth <= 768) return 500; // Tablets
+  if (window.innerWidth <= 1024) return 650; // Medium Screens
+  return 800; // Default for Desktop
+};
+
 // creating dot btn with js dom
 const button = document.querySelector(".button");
 for(let i = 0; i<length; i++){
@@ -45,24 +53,31 @@ const changeColor = () => {
 // this is for arrow btn...
 // create a function for nxt slide.....
 const nextSlide = () => { //fun for right arrow
-  slider.style.transform = `translateX(-${slideNumber * 800}px)`; //then add css style->property(transform)...size of img(width =800px)..so multiply slidenumber with 800px..then img will change
+  const imgWidth = getImageWidth(); // Get width dynamically
+  // slider.style.transform = `translateX(-${slideNumber * 800}px)`; //then add css style->property(transform)...size of img(width =800px)..so multiply slidenumber with 800px..then img will change
+  slider.style.transform = `translateX(-${slideNumber * imgWidth}px)`;
   slideNumber++;
 };
 
 const prevSlide = () => { //fun for left arrow
-    slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`; // slideNumber - 2*800 mean 1 slide back
+  const imgWidth = getImageWidth(); // Get width dynamically
+    // slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`; // slideNumber - 2*800 mean 1 slide back
+    slider.style.transform = `translateX(-${(slideNumber - 2) * imgWidth}px)`;
     slideNumber--;
   };
 
 // for returning in 1st slide
 const getFirstSlide = () => { //for right arrow
+  // const imgWidth = getImageWidth(); // Get width dynamically
   slider.style.transform = `translateX(0px)`; //when it goes on last img...then 0px change...mean 1st img
   slideNumber = 1;
 };
 
 // for go back on last slide
 const getLastSlide = () => { //for left arrow
-    slider.style.transform = `translateX(-${(length - 1)*800}px)`; //mean total length - 1 *800 mean back 1 slide
+  const imgWidth = getImageWidth(); // Get width dynamically
+    // slider.style.transform = `translateX(-${(length - 1)*800}px)`; //mean total length - 1 *800 mean back 1 slide
+    slider.style.transform = `translateX(-${(length - 1) * imgWidth}px)`;
     slideNumber = length; //it will go back to total length mean length of slide
   };
 
